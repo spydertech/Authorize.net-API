0b25618799798ca1cc26339bbf5f2fcf75bdc0ea
Spyder Tech Web Design <d.berkwits@spydertechonline.com>
2017-09-27 11:46:43 -0400

    added bootstrap and session_start()

--- index.php
+++ index.php
@@ -1,135 +1,74 @@
-<div class="container main-content">
-	<div class="row">
-		<div class="col-lg-12">
-			<form name="submit_payment" id="submit_payment" method="post" action="payment.php" enctype="multipart/form-data">
-			<p id="response" align="center"></p>
-			<div class="vc_col-sm-7">
-				<h2>credit card details</h2>
-				<div class="row">
-					<div class="vc_col-sm-12">
-						<div class="section-part">
-							<div class="vc_col-sm-4">
-								Amount
-							</div>
-							<div class="vc_col-sm-8">
-								<span>
-									<input type="text" name="finalPrice" value="" />
-								</span>
-							</div>
-						</div>
-						<!-- <div class="section-part">
-							<div class="vc_col-sm-4">
-								Auto Renew
-							</div>
-							<div class="vc_col-sm-8">
-								<span>
-									<input type="checkbox" name="arb" id="arb" value="Y">
-								</span>
-							</div>
-						</div> -->
-						<div class="section-part">
-							<div class="vc_col-sm-4">
-								Card Number
-							</div>
-							<div class="vc_col-sm-8">
-								<span>
-									<input type="text" name="ccnumber" id="ccnumber" style="width:200px" value="" size="40">
-								</span>
-							</div>
-						</div>
-						<div class="section-part">
-							<div class="vc_col-sm-4">
-								Exp Date (MM/YY)
-							</div>
-							<div class="vc_col-sm-8">
-								<span>
-									<input type="text" name="expdate" id="expdate" value="" style="width:80px" size="40">
-								</span>
-							</div>
-						</div>
-						<div class="section-part">
-							<div class="vc_col-sm-4">
-								CCV
-							</div>
-							<div class="vc_col-sm-8">
-								<span>
-									<input type="text" name="ccv" id="ccv" value="" size="40" style="width:80px">
-								</span>
-							</div>
-						</div>
-						<div class="section-part">
-							<div class="vc_col-sm-4">
-								First Name
-							</div>
-							<div class="vc_col-sm-8">
-								<span>
-									<input type="text" name="fname" id="fname" value="" size="40">
-								</span>
-							</div>
-						</div>
-						<div class="section-part">
-							<div class="vc_col-sm-4">
-								Last Name
-							</div>
-							<div class="vc_col-sm-8">
-								<span>
-									<input type="text" name="lname" id="lname" value="" size="40">
-								</span>
-							</div>
-						</div>
-						<div class="section-part">
-							<div class="vc_col-sm-4">
-								Address
-							</div>
-							<div class="vc_col-sm-8">
-								<span>
-									<input type="text" name="address" id="address" value="" size="40">
-								</span>
-							</div>
-						</div>
-						<div class="section-part">
-							<div class="vc_col-sm-4">
-								City
-							</div>
-							<div class="vc_col-sm-8">
-								<span>
-									<input type="text" name="city" id="city" value="" size="40">
-								</span>
-							</div>
-						</div>
-						<div class="section-part">
-							<div class="vc_col-sm-4">
-								State
-							</div>
-							<div class="vc_col-sm-8">
-								<span>
-									<input type="text" name="state" id="state" value="" size="40">
-								</span>
-							</div>
-						</div>
-						<div class="section-part">
-							<div class="vc_col-sm-4">
-								Zip
-							</div>
-							<div class="vc_col-sm-8">
-								<span>
-									<input type="text" name="zip" id="zip" value="" size="40">
-								</span>
-							</div>
-						</div>
-						<div class="section-part">
-							<div class="vc_col-sm-4"></div>
-							<div class="vc_col-sm-8">
-								<span>
-									<input class="btn btn-primary" type="submit" name="submit_payment" value="Submit Payment">
-								</span>
-							</div>
-						</div>
-						<input type="hidden" name="mode" value="process" />
-						
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+<?php session_start(); ?>
+ 
+<html>
+    <head>
+        <title>Authorize.net Credit Card Form</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    </head>
+    <body>
+         
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <!-- -->
+                </div>
+                <div class="col-sm-6">
+                    <h2>Credit Card Details</h2>
+                    <form name="cc_payment" id="cc_payment" method="post" action="payment.php" enctype="multipart/form-data">
+                        <input type="hidden" name="mode" value="process" />
+                        <div class="form-group">
+                            <label>Payment Amount</label>
+                            <input class="form-control" type="text" name="amount_paid" />
+                        </div>
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-sm-6" style="padding-right: 2px;">
+                                    <label>First Name</label>
+                                    <input class="form-control" type="text" name="cc_fname" />
+                                </div>
+                                <div class="col-sm-6" style="padding-left: 2px;">
+                                    <label>Last Name</label>
+                                    <input class="form-control" type="text" name="cc_lname" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-sm-6" style="padding-right: 2px;">
+                                    <label>Card Number</label>
+                                    <input class="form-control" type="text" name="cc_number" />
+                                </div>
+                                <div class="col-sm-3" style="padding-right: 2px; padding-left: 2px;">
+                                    <label>Exp (MM/YY)</label>
+                                    <input class="form-control" type="text" name="cc_exp" />
+                                </div>
+                                <div class="col-sm-3" style="padding-left: 2px;">
+                                    <label>CCV</label>
+                                    <input class="form-control" type="text" name="cc_ccv" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <input type="checkbox" name="auto_renew" id="auto_renew" value="Y" />
+                            <label for="auto_renew">Yes, make payment automatically each month.</label>
+                        </div>
+                        <div class="form-group">
+                            <?php
+                                echo  $_SESSION['msg'];
+                                unset($_SESSION['msg']);
+                            ?>
+                            <input class="btn btn-primary" type="submit" name="cc_payment_submit" value="Submit Payment">
+                        </div>
+                    </form>
+                </div>
+                <div class="col-sm-3">
+                    <!-- -->
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
